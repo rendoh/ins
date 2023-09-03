a39461ee87c6e22425130c879d12431bd6319b9b
Ryohei Endo <r-endo@studio-spoon.co.jp>
2023-09-03 17:57:50 +0900

    add lint rules and add formatter

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,4 +1,14 @@
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
-  extends: ["@remix-run/eslint-config", "@remix-run/eslint-config/node"],
+  extends: [
+    '@remix-run/eslint-config',
+    '@remix-run/eslint-config/node',
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+  ],
+  plugins: ['simple-import-sort'],
+  rules: {
+    'simple-import-sort/imports': 'warn',
+    'simple-import-sort/exports': 'warn',
+  },
 };
